1c702e83600099ddcef147932f8125fd0861cb5a
RamSailopal <thelinuxguys@yahoo.com>
2022-05-20 12:32:40 +0100

    New zip file

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y wget unzip 
 cd /tmp
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=13Qz5usuqPZxZWE3ZlqpImSBYAcp5i6fL&confirm=t' -O 'yottadbmetric.zip'
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=17EQMzWEC42ob5TqhBoL8ynFm4Cli7JlG&confirm=t' -O 'yottadbmetric.zip'
 sed -i 's/^;allow_loading_unsigned_plugins =/allow_loading_unsigned_plugins = ramsailopal-yottadb-datasource/' /etc/grafana/grafana.ini
 sed -i 's/^;enable_gzip = false/enable_gzip = true/' /etc/grafana/grafana.ini
 cd /var/lib/grafana/plugins
